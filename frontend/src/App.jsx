560be3dd2127215849dd4f5adc915bbde19dbd88
Fernando <ferg.developer@gmail.com>
2024-09-28 16:44:36 -0400

    added routes to each component in FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Dashboard } from './pages/Dashboard';
+import { ComponentsLibrary } from './pages/ComponentsLibrary';
+import { Boilerplates } from './pages/Boilerplates';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
+import { Navbar } from './components/Navbar';
 
-export default App
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/components" element={<ComponentsLibrary/>} />
+        <Route path="/boilerplates" element={<Boilerplates/>} />
+      </Routes>
+    </div>
+  );
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>home</li>
+        <li>tab1</li>
+        <li>tab2</li>
+      </ul>
+    </div>
+  )
+}
